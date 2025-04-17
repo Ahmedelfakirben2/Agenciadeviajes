59250e94f3a17f4efb5875c2df39ac661bb41202
ahmedelfakirben <ahmedelfakirben@gmail.com>
2025-04-17 15:15:38 +0000

    migracion

--- migrations/20250417150416-create-hotel-table.js
+++ migrations/20250417150416-create-hotel-table.js
@@ -9,6 +9,65 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('hoteles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      direccion: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ciudad: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      pais: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      estrellas: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5
+        }
+      },
+      telefono: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
+      },
+      sitio_web: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      imagenes: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
@@ -18,5 +77,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable('hoteles');
   }
 };
